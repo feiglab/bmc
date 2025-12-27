c1aeff4d2c5049b0555d9902853ccb5c3f5ee06a
Michael Feig <mfeiglab@gmail.com>
2025-12-27 11:28:37 -0500

    new script

---
+++ scripts/setupprod.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+nsteps=${1:-1250000}
+maxrun=${2:-10}
+
+if [[ -n "${3:-}" && -d "$3" ]]; then
+    cd -- "$3"
+fi
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+template=${script_dir}/job.bmctileumbrella.slurm.template
+
+if [[ ! -r $template ]]; then
+   echo "cannot find template"
+   exit 1
+fi
+
+tag=US
+[[ -r tag ]] && tag=$(<tag)
+
+dir=$(pwd)
+
+dir_esc=$(printf '%s' "$dir" | sed 's/[\/&|\\]/\\&/g')
+
+echo $maxrun > maxrun
+for n in run_?.??; do
+  biasval=${n#run_}
+  echo 0 > "$n/last"
+
+  sed \
+    -e "s/BIAS/$biasval/g" \
+    -e "s/BDIR/$n/g" \
+    -e "s|DIR|$dir_esc|g" \
+    -e "s/TAG/$tag/g" \
+    -e "s/NSTEPS/$nsteps/g" \
+    $template > "$n/job.prodbias.slurm"
+done
+
